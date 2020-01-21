1842a0599b11ac16aca240407a5e5b719ead5d13
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-01-21 16:29:22 +0800

    Initial commit

---
+++ 範例程式 (1)/範例程式/Ch16PHP/getDateTime.php
@@ -0,0 +1,17 @@
+﻿<?php
+// XML文件
+header("Content-Type: text/xml");
+// 取得欄位值
+$name = (isset($_POST["name"]) ) ? $_POST["name"] : $_GET["name"];
+$type = (isset($_POST["type"]) ) ? $_POST["type"] : $_GET["type"];
+echo "<?xml version=\"1.0\" ?>";
+if ($type == "date")
+   $output = date("m/j/Y");
+else
+   $output = date("h:i:s A");
+echo "<datetime>"; 
+echo "<name>" . $name . "</name>";
+echo "<data>" . $output . "</data>";
+echo "</datetime>";
+?> 
+
